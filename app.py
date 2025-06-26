825a82529ff4dac903ef1fb0ee33e9f156e329c1
Raj Lakhanpal <rajlakhanpal0126@gmail.com>
2025-06-25 21:38:44 -0700

    Add Flask app for deployment

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+from math_utils import add
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return jsonify({"message": "CI/CD Demo working!"})
+
+@app.route("/add/<int:a>/<int:b>")
+def add_route(a, b):
+    return jsonify({"result": add(a, b)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
